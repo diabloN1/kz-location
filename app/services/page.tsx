fd39bcc1bba102d7dc795aa26ca8ef38cf8c6a04
DiaBlo <amines3idia@gmail.com>
2024-07-15 01:06:36 +0100

    fix services vercel error

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,24 +1,24 @@
-// "use client"
+"use client"
 
-// import React from "react"
+import React from "react"
 
-// import Dashboard from "@/components/SIDEBAR"
+import Dashboard from "@/components/SIDEBAR"
 
-// export default function ServicesPage() {
-//   return (
-//     <div className="flex flex-col items-center p-4">
-//       <div className="flex flex-row justify-center w-full space-x-4">
-//         <h1 className="sm:mt-10 text-4xl font-bold md leading-tight tracking-tighter md:text-xl xl:text-4xl sm:text-2xl xs:text-xl">
-//           Services fournis
-//         </h1>
-//         <p className="w-[100%] lg:w-2/3 text-left mt-10 xs:w-[100%] lg:max-w-[600px] text-lg text-muted-foreground">
-//           Veuillez cliquer sur le service souhaité et envoyer un message
-//           décrivant vos besoins.
-//         </p>
-//       </div>
+export default function ServicesPage() {
+  return (
+    <div className="flex flex-col items-center p-4">
+      <div className="flex flex-row justify-center w-full space-x-4">
+        <h1 className="sm:mt-10 text-4xl font-bold md leading-tight tracking-tighter md:text-xl xl:text-4xl sm:text-2xl xs:text-xl">
+          Services fournis
+        </h1>
+        <p className="w-[100%] lg:w-2/3 text-left mt-10 xs:w-[100%] lg:max-w-[600px] text-lg text-muted-foreground">
+          Veuillez cliquer sur le service souhaité et envoyer un message
+          décrivant vos besoins.
+        </p>
+      </div>
 
-//       <br />
-//       <Dashboard />
-//     </div>
-//   )
-// }
+      <br />
+      <Dashboard />
+    </div>
+  )
+}
